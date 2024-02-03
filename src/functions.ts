a2ea017e5d4d88779daa3b91b65f6d47d891f3d6
palacharlanarendra <palacharlanarendra@gmail.com>
2024-02-04 03:34:50 +0530

    fixed the readme file

--- src/functions.ts
+++ src/functions.ts
@@ -9,35 +9,37 @@ export function base64ToJSON({
   base64CsvData,
   type,
 }: Base64ToJSONProps): Row[] {
-  if (type === "xlsx") {
-    const base64String = base64CsvData;
-    const decodedData = Buffer.from(base64String, "base64");
-    const workbook = xlsx.read(decodedData, { type: "buffer" });
-    const jsonData: Row[] = xlsx.utils.sheet_to_json(
-      workbook?.Sheets?.[workbook?.SheetNames?.[0]]
-    );
-    return jsonData;
-  } else if (type === "csv") {
-    const base64String = base64CsvData;
-    const binaryData = atob(base64String);
-    const csvString = binaryData;
-    const rows = csvString.split("\n");
-    const headers = rows[0]
-      .split(",")
-      .map((header) => header?.replaceAll('"', "")?.trim());
+  const base64String = base64CsvData;
+  const decodedData = Buffer.from(base64String, "base64");
 
-    const jsonData: Row[] = [];
-    for (let i = 1; i < rows.length; i++) {
-      const row = rows[i].split(",");
-      const obj: Row = {};
-      for (let j = 0; j < headers.length; j++) {
-        obj[headers[j]] = row[j]?.replaceAll('"', "").trim();
-      }
-      jsonData.push(obj);
-    }
-    return jsonData;
+  switch (type) {
+    case "xlsx":
+      const workbook = xlsx.read(decodedData, { type: "buffer" });
+      return (
+        xlsx.utils.sheet_to_json(
+          workbook?.Sheets?.[workbook?.SheetNames?.[0]]
+        ) || []
+      );
+
+    case "csv":
+      const binaryData = atob(base64String);
+      const csvString = binaryData;
+      const [headerLine, ...dataLines] = csvString.split("\n");
+      const headers = headerLine
+        .split(",")
+        .map((header) => header.replace(/"/g, "").trim());
+      const jsonData: Row[] = dataLines.map((line) => {
+        const row = line
+          .split(",")
+          .map((item) => item.replace(/"/g, "").trim());
+        return headers.reduce((obj, header, index) => {
+          obj[header] = row[index];
+          return obj;
+        }, {} as Row);
+      });
+      return jsonData;
+
+    default:
+      return [];
   }
-
-  // Default return statement to handle cases where type is neither "xlsx" nor "csv"
-  return [];
 }
